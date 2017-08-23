e2ed4014af2ef5eb0b23e82def9b7712a38ab9b7
Alicia Wilkin <aliciadwilkin@gmail.com>
2017-08-23 17:49:58 -0600

    initial commit dangit

---
+++ assets/scripts/eqcss.js
@@ -0,0 +1,58 @@
+@element ".Masthead-wrap" and (max-width: 220px) {
+  .Masthead-wrap h1 {
+    font-size: 36px;
+  }
+
+  .Masthead-wrap p {
+    font-size: 20px;
+  }
+}
+
+@element ".Masthead-wrap" and (min-width: 221px) and (max-width: 399px) {
+  .Masthead-wrap h1 {
+    font-size: 45px;
+  }
+
+  .Masthead-wrap p {
+    font-size: 20px;
+  }
+}
+
+@element ".Masthead-wrap" and (min-width: 400px) and (max-width: 889px) {
+  .Masthead-wrap h1 {
+    font-size: 63px;
+  }
+
+  .Masthead-wrap p {
+    font-size: 28px;
+  }
+}
+
+@element ".CaseStudy-description" and (max-width: 214px) {
+  .CaseStudy-description .title {
+    font-size: 26px;
+    line-height: 1.2;
+  }
+
+  .CaseStudy-description .intro {
+    font-size: 16px;
+  }
+
+  .CaseStudy-description .Button {
+    font-size: 14px;
+  }
+}
+
+@element ".CaseStudy-description" and (min-width: 215px) and (max-width: 329px) {
+  .CaseStudy-description .title {
+    font-size: 35px;
+  }
+
+  .CaseStudy-description .intro {
+    font-size: 20px;
+  }
+
+  .CaseStudy-description .Button {
+    font-size: 16px;
+  }
+}
