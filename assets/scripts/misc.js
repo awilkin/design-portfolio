d860515518e86f925e100dbbe019df72631cec42
Alicia Wilkin <aliciadwilkin@gmail.com>
2017-08-24 22:32:58 -0600

    hover working

--- assets/scripts/misc.js
+++ assets/scripts/misc.js
@@ -2,14 +2,10 @@ $(function(){
 
   var bricklayer = new Bricklayer(document.querySelector('.bricklayer'))
 
-
-// on hover over project img, nearest card will show
-// document.getElementById('gform').style.display = 'none'; // hide form
-
   // hover on a link card, card will show
   $("article").hover(function() {
-    $(".card").css("opacity", "1");
-    // this.children(".card").css("opacity", "1");
+    $(this).children(".card").fadeToggle(100);
   }); // hover function
+
 
 }); //ready
