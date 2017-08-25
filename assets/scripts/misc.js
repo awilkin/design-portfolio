a311df4003883bc6ac10db8d1c9dbfa4032754bb
Alicia Wilkin <aliciadwilkin@gmail.com>
2017-08-24 21:30:33 -0600

    fixed styling issues, added js file for card displays

--- assets/scripts/misc.js
+++ assets/scripts/misc.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
+$(function(){
 
   var bricklayer = new Bricklayer(document.querySelector('.bricklayer'))
+
+
+// on hover over project img, nearest card will show
+// document.getElementById('gform').style.display = 'none'; // hide form
+
+  // hover on a link card, card will show
+  $("article").hover(function() {
+    $(".card").css("opacity", "1");
+    // this.children(".card").css("opacity", "1");
+  }); // hover function
 
 }); //ready
