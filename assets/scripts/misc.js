f70158241581fe5ef18011ff706d574fd0beecd6
Alicia Wilkin <aliciadwilkin@gmail.com>
2017-09-05 22:33:45 -0600

    hover is not default. bad idea Ali. added olumo and whosdown.html

--- assets/scripts/misc.js
+++ assets/scripts/misc.js
@@ -2,10 +2,10 @@ $(function(){
 
   var bricklayer = new Bricklayer(document.querySelector('.bricklayer'))
 
-  // hover on a link card, card will show
-  $("article").hover(function() {
-    $(this).children(".card").fadeToggle(100);
-  }); // hover function
+  // // hover on a link card, card will show
+  // $("article").hover(function() {
+  //   $(this).children(".card").fadeToggle(100);
+  // }); // hover function
 
 
 }); //ready
